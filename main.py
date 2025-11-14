40b9596f2a0912a21f3d41075e0cf26e9c72ba6b
Toomuchtosay <tomuchtosay16@gmail.com>
2025-11-14 17:33:38 -0500

    B: added a circleclass and a player class file that are used to and called in the pygame file to create the simple player moduel with a show triangle but an active cirlce for hitbox

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import constants as c
 from logger import log_state
+import player as p
 
 def main():
     pygame.init()
@@ -12,17 +13,23 @@ def main():
     print(f'Screen width: {c.SCREEN_WIDTH}')
     print(f'Screen height: {c.SCREEN_HEIGHT}')
     screen = pygame.display.set_mode((c.SCREEN_WIDTH, c.SCREEN_HEIGHT))
-
-    #call game loop#
+    
+       #call game loop#
     pygame.running = True
     while pygame.running:
         log_state()
+        
+
         ##pygame quit stuff##
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         ##ends quit check##
         screen.fill('black')
+  #create player object#
+        player = p.Player(c.SCREEN_WIDTH/2, c.SCREEN_HEIGHT/2, c.PLAYER_RADIUS)
+        player.draw(screen)
+
         pygame.display.flip()
         delta_time = clock.tick(60)
         
