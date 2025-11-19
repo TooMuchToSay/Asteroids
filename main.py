ac9bd01e62f8d6e1a31d57bf08accaf06e42ae2d
Toomuchtosay <tomuchtosay16@gmail.com>
2025-11-19 10:41:12 -0500

    C feature: added a unit vector update fuction in the player class that updates the location of the player on the screen by taking in account their rotation and then moving either backwards or forwards based on the input of w or s. I need to learn math

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ def main():
     print(f'Screen width: {c.SCREEN_WIDTH}')
     print(f'Screen height: {c.SCREEN_HEIGHT}')
     screen = pygame.display.set_mode((c.SCREEN_WIDTH, c.SCREEN_HEIGHT))
-    
+    player = p.Player(c.SCREEN_WIDTH/2, c.SCREEN_HEIGHT/2, c.PLAYER_RADIUS)
+
        #call game loop#
     pygame.running = True
     while pygame.running:
@@ -27,11 +28,14 @@ def main():
         ##ends quit check##
         screen.fill('black')
   #create player object#
-        player = p.Player(c.SCREEN_WIDTH/2, c.SCREEN_HEIGHT/2, c.PLAYER_RADIUS)
+
+        delta_time = clock.tick(60) / 1000
+        player.update(delta_time)
         player.draw(screen)
+       
 
         pygame.display.flip()
-        delta_time = clock.tick(60)
+       
         
 if __name__ == "__main__":
     main()
--- player.py
+++ player.py
@@ -2,11 +2,14 @@ import pygame
 from circleshape import CircleShape
 from constants import PLAYER_RADIUS
 from constants import LINE_WIDTH
+from constants import PLAYER_TURN_SPEED
+from constants import PLAYER_SPEED
 
 class Player(CircleShape):
     def __init__(self, x, y, PLAYER_RADIUS):
         super().__init__(x, y, PLAYER_RADIUS) #PARENT CONSTRUCTOR``
         self.rotation = 0
+        self.position = pygame.math.Vector2(x,y)
 
     def draw(self, screen):
         pygame.draw.polygon(screen, 'white', self.triangle(), LINE_WIDTH)
@@ -20,5 +23,23 @@ class Player(CircleShape):
         c = self.position - forward * self.radius + right
         return [a, b, c]
 
+    def rotate(self, dt): #turns the player by count pattern
+        self.rotation += PLAYER_TURN_SPEED * dt
+        print(self.rotation) 
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
 
+        if keys[pygame.K_a]:
+            self.rotate(-dt) #reverses it
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
 
+    def move(self, dt):
+        uv = pygame.math.Vector2(0,1)
+        rotated_vector = uv.rotate(self.rotation)
+        rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
+        self.position += rotated_with_speed_vector
